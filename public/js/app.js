5e31b6b62799437be9fd5eac8a94acd1685a3c8b
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-06-20 21:47:05 -0300

    Decida Por Mim Querido Note - Foundation Stone(Babel / React)

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var template = React.createElement(
+  "h1",
+  { className: "teste" },
+  "Decide Por Mim, Querido Note"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+let template = <h1 className="teste">Decide Por Mim, Querido Note</h1>;
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot); 
