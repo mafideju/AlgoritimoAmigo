5b09733ac423b82143f3a0c9721315efc2ec4898
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-08-15 22:00:59 -0300

    Reboot it

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Are You Ready To Go?</h2>
+      </div>
+    );
+  }
+}
+export default App;
